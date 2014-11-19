48c37266b014a2fad3ebb35e4eb87e36d745f8e1
Jade Dominguez <plusjade@gmail.com>
2014-11-19 01:58:36 -0800

    Locked down the lessons controller.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,15 @@
 class LessonsController < ApplicationController
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
   def index
+    @lessons = Lesson.all
+    authorize! :read, Lesson
   end
 
   def show
     @lesson = Lesson.where(lesson: params[:id].to_f ).first
+    authorize! :read, @lesson
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,5 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user
+      can :read, Lesson
+      can :manage, Feedback, student: user
+    end
   end
 end
