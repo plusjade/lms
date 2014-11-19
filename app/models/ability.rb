60a521890ebb0b8b082e7b1ee2de9df8c5d82999
Jade Dominguez <plusjade@gmail.com>
2014-11-19 02:29:28 -0800

    Use Single Table Inheritance for Teacher, Student, User.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user
+    case user
+    when Teacher
+      can :manage, :all
+    when Student
       can :read, Lesson
       can :manage, Feedback, student: user
     end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,7 @@
-class Student
+class Student < User
   include Mongoid::Document
   include Mongoid::Timestamps
 
   has_many :feedbacks
   has_many :attendances
-
-  field :name, type: String
-  field :email, type: String
-  field :avatar, type: String
 end
