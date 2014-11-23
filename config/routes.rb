787e327506221443544ca72827eed667201df618
Jade Dominguez <plusjade@gmail.com>
2014-11-23 15:54:12 -0800

    Remove ability to create students from the UI.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :attendances
 
   resources :feedbacks
-  resources :students do
+  resources :students, only: [:index] do
     resources :lessons do
       member do
         get 'feedback' => 'feedbacks#show'
