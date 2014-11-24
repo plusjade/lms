835a3c08853b70b63d26d4d92a5fbc44437fdf0a
Jade Dominguez <plusjade@gmail.com>
2014-11-24 02:55:43 -0800

    Add dependent destroy on associations.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,6 @@ class Student < User
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  has_many :feedbacks
-  has_many :attendances
+  has_many :feedbacks, dependent: :destroy
+  has_many :attendances, dependent: :destroy
 end
