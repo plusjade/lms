41c338bd21225e7d572d6f195e2dc842678ad1b8
Jade Dominguez <plusjade@gmail.com>
2014-11-28 04:23:17 -0800

    List courses on homepage or redirect if only one.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @courses = current_user.courses
+    if @courses.count == 1
+      redirect_to course_path(@courses.first)
+    end
   end
 end
