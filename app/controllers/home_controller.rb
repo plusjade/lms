79836a858caf540535461e416e13952de800610a
Jade Dominguez <plusjade@gmail.com>
2014-12-03 00:52:59 -0800

    Split logged in home screen vs logged-out.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @courses = current_user.courses
-    if @courses.count == 1
-      redirect_to course_path(@courses.first)
+    if current_user
+      @courses = current_user.courses
+      if @courses.count == 1
+        redirect_to course_path(@courses.first)
+      end
+    else
+      render template: "home/login"
     end
   end
 end
